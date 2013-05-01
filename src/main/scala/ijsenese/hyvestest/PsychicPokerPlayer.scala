ea0004daf99503ab3fc679cd70932f689434ef22
Ivo Senese <ivo.senese@videoplaza.com>
2013-05-01 03:34:03 +0200

    modified comment

--- src/main/scala/ijsenese/hyvestest/PsychicPokerPlayer.scala
+++ src/main/scala/ijsenese/hyvestest/PsychicPokerPlayer.scala
@@ -3,7 +3,7 @@ package ijsenese.hyvestest
 object PsychicPokerPlayer {
 
     /*
-    * Returns the rank of the poker one can get given the starting hand and the cards on the deck
+    * Returns the rank of the poker hand one can get given the starting hand and the cards on the deck
     * */
     def getBestHand(handCards: List[Card], deckCards: List[Card]) = {
         val ranks = for {
